98cc6f784e83626d4acdc05b1388233f96e20f4b
ShingetsuStudios <wbhashman@yahoo.com>
2022-11-11 03:15:00 -0500

    Download Button
    I am so smart I swear

--- characterSelectBuilder.js
+++ characterSelectBuilder.js
@@ -71,6 +71,9 @@ $(document).ready(function () {
     $('#copyCode').on('click', function () {
         copyToClipboard(character)
     })
+    $('#downloadCode').on('click', function() {
+        downloadJSON()
+    })
     $('#modeChange').on('click', function () {
         if (mode == "build") {
             mode = "view"
@@ -537,4 +540,43 @@ function adjust(x, n, char) {
     } else {
         return x + n
     }
+}
+
+function downloadJSON() {
+
+    var textFile = null,
+        makeTextFile = function (text) {
+            var data = new Blob([text], {
+                type: 'text/javascript'
+            });
+
+            // If we are replacing a previously generated file we need to
+            // manually revoke the object URL to avoid memory leaks.
+            if (textFile !== null) {
+                window.URL.revokeObjectURL(textFile);
+            }
+
+            textFile = window.URL.createObjectURL(data);
+
+            return textFile;
+        };
+
+    var create = document.getElementById('downloadCode')
+    //textbox = document.getElementById('textbox');
+
+    create.addEventListener('click', function () {
+        var link = document.createElement('a');
+        link.setAttribute('download', 'character.js');
+        link.href = makeTextFile("var character = " + JSON.stringify(character, null, 2));
+        document.body.appendChild(link);
+
+        // wait for the link to be added to the document
+        window.requestAnimationFrame(function () {
+            var event = new MouseEvent('click');
+            link.dispatchEvent(event);
+            document.body.removeChild(link);
+        });
+
+    }, false);
+
 }
